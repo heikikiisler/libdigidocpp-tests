1514374ce3f2ba01f2cb9ebbe8aada4d5b8b7027
Margus Härm <margus.harm@testijad.ee>
2018-01-23 18:46:25 +0200

    Template class for Http server.

---
+++ src/main/java/ee/testijad/mobilecpp/server/HttpServer.java
@@ -0,0 +1,25 @@
+package ee.testijad.mobilecpp.server;
+
+public class HttpServer {
+
+    public HttpServer(int port) {
+        startServer(port);
+    }
+
+    private void startServer(int port) {
+       // TODO
+    }
+
+    private void receiveFile(String fileName) {
+
+    }
+
+    private void sendFile(String fileName) {
+
+    }
+
+    public void stopServer() {
+        //TODO
+
+    }
+}
