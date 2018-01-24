190e9b11160b1eb61ed1e49255b0ed77d6c43090
Margus Härm <margus.harm@testijad.ee>
2018-01-24 14:41:02 +0200

    HttpServer modified.

--- src/main/java/ee/testijad/mobilecpp/server/HttpServer.java
+++ src/main/java/ee/testijad/mobilecpp/server/HttpServer.java
@@ -1,14 +1,78 @@
 package ee.testijad.mobilecpp.server;
 
+import ee.testijad.mobilecpp.util.Utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
 
-    public HttpServer(int port) {
-        startServer(port);
+    public static void main(String[] args) throws InterruptedException {
+        HttpServer server = new HttpServer(Utils.getFreePort());
+        server.startServer();
+        System.out.println("Started server");
+        Thread.sleep(5000);
+        server.stopServer();
     }
 
-    private void startServer(int port) {
-       // TODO
+    private ServerSocket socket = null;
+    private Thread thread;
+    private Runnable task;
+    private int port;
+
+
+    public HttpServer(int port) {
+        this.port = port;
     }
+
+    private void startServer() {
+        try {
+            socket = new ServerSocket(port);
+            thread = new Thread(task);
+            thread.run();
+        } catch (IOException e) {
+            System.err.println("Could not start server: " + e);
+            System.exit(-1);
+        }
+        System.out.println("FileServer accepting connections on port " + port);
+        Socket connection;
+        try {
+            connection = socket.accept();
+            task = new Runnable() {
+                @Override
+                public void run() {
+                    HandleRequest(connection);
+                }
+            };
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void HandleRequest(Socket s) {
+        BufferedReader in;
+        PrintWriter out;
+        String request;
+
+        try {
+            s.close();
+        } catch (IOException e) {
+            System.out.println("Failed respond to client request: " + e.getMessage());
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     private void receiveFile(String fileName) {
 
@@ -19,7 +83,7 @@ public class HttpServer {
     }
 
     public void stopServer() {
-        //TODO
-
+        System.out.println("Stopping server");
+        thread.interrupt();
     }
 }
