aebbbd4b781938b4364dac7000d9cae73e312e95
heiki.kiisler <heiki.kiisler@gmail.com>
2018-01-26 15:25:29 +0200

    Added timeout implementation for polling results; Added warning message about error

--- src/main/java/ee/testijad/mobilecpp/action/Action.java
+++ src/main/java/ee/testijad/mobilecpp/action/Action.java
@@ -5,14 +5,11 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-import java.util.logging.LogManager;
+import java.time.Instant;
 
 public class Action {
 
@@ -20,11 +17,21 @@ public class Action {
     private static final By TEXT_VIEW = MobileBy.xpath(String.format("//android.widget.TextView[@resource-id='%s']", LOCATOR));
 
     public static void waitForResult(AppiumDriver driver, int timeoutInSeconds) {
+        Instant startTime = Instant.now();
+        Instant endTime = startTime.plusSeconds(timeoutInSeconds);
         while (true) {
             try {
                 new WebDriverWait(driver, timeoutInSeconds).until(ExpectedConditions.visibilityOfElementLocated(TEXT_VIEW));
-            } catch (WebDriverException e) {
-                continue;
+                // Can not catch or avoid UiAutomator2Exception of 10000 ms
+            } catch (WebDriverException ignored) {
+                if (Instant.now().isBefore(endTime)) {
+                    System.err.println("##########################################################");
+                    System.err.println("#### Ignore above exception, still polling for result ####");
+                    System.err.println("##########################################################");
+                    continue;
+                } else {
+                    throw new RuntimeException(String.format("Reached defined timeout of %s seconds while waiting for result", timeoutInSeconds));
+                }
             }
             break;
         }
